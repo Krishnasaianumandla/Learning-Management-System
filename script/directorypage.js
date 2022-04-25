cdeafe9e35be9cc01c49306512c5a98f3ec1d634
Krishnasaianumandla <krishnasaieee1997@gmail.com>
2022-04-25 18:41:24 +0530

    made view details button anchor tag

--- script/directorypage.js
+++ script/directorypage.js
@@ -62,7 +62,7 @@ export  async function fetchdata(){
                   <h4 class="card-text">${args[i].name}</h4>
                   <h6 class="card-text">${args[i].email}</h6>
                   <h6 class="card-text"> ${args[i].bio}</h6>
-                  <a href="./new_user_registration.html" target="_blank" ><button  onclick="progress('${args[i].name}')">View details</button></a>                
+                  <a href="./new_user_registration.html" target="_blank" onclick="progress('${args[i].name}')" type="button" class="btn btn-primary">View details</a>              
                 </div>
          </div>`
      }
