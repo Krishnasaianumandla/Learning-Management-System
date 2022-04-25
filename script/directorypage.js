aa89b373142b2bce4a99a59edcff20cb0a84a4a9
Krishnasaianumandla <krishnasaieee1997@gmail.com>
2022-04-25 18:48:27 +0530

    ...

--- script/directorypage.js
+++ script/directorypage.js
@@ -62,7 +62,7 @@ export  async function fetchdata(){
                   <h4 class="card-text">${args[i].name}</h4>
                   <h6 class="card-text">${args[i].email}</h6>
                   <h6 class="card-text"> ${args[i].bio}</h6>
-                  <a href="./learners_progress_page.html" target="_blank" onclick="progress('${args[i].name}')" type="button" class="btn btn-primary">View details</a>              
+                  <a href="/learners_progress_page.html" target="_blank" onclick="progress('${args[i].name}')" type="button" class="btn btn-primary">View details</a>              
                 </div>
          </div>`
      }
